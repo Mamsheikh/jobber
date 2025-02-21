2757a6a1a0fd1c9fe6db5558379737fbcba3eb7c
Munir Ali Mohammed <mamsheikh123@gmail.com>
2025-02-21 15:46:43 +0530

    feat: graphql setup and users module

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true
+    }),
+    UsersModule
+  ],
+  providers: []
 
 })
 export class AppModule {}

79bfcc390cd8b879c9371bb6bc2c9175dfd19d7c
Munir Ali Mohammed <mamsheikh123@gmail.com>
2025-02-24 01:07:13 +0530

    feat: jobs resolver

--- apps/jobber-jobs/src/app/app.module.ts
+++ apps/jobber-jobs/src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { JobsModule } from './jobs/jobs.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ConfigModule, JobsModule],
+  imports: [
+    ConfigModule,
+    JobsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+    }),
+  ],
 })
 export class AppModule {}
---
+++ apps/jobber-jobs/src/app/jobs/jobs.service.ts
@@ -0,0 +1,33 @@
+import {
+  DiscoveryService,
+  DiscoveredClassWithMeta,
+} from '@golevelup/nestjs-discovery';
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
+import { JOB_METADATA_KEY } from '../decorators/job.decorator';
+import { AbstractJob } from './jobs';
+import { JobMetadata } from '../interfaces/job-metadata.interface';
+
+@Injectable()
+export class JobsService implements OnModuleInit {
+  private jobs: DiscoveredClassWithMeta<JobMetadata>[] = [];
+  constructor(private readonly discoveryService: DiscoveryService) {}
+
+  async onModuleInit() {
+    this.jobs = await this.discoveryService.providersWithMetaAtKey<JobMetadata>(
+      JOB_METADATA_KEY
+    );
+  }
+
+  getJobs() {
+    return this.jobs.map((job) => job.meta);
+  }
+
+  async executeJob(name: string) {
+    const job = this.jobs.find((job) => job.meta.name === name);
+    if (!job) {
+      throw new BadRequestException(`Job ${name} does not exist.`);
+    }
+    await (job.discoveredClass.instance as AbstractJob).execute();
+    return job.meta;
+  }
+}

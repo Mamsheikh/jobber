f7bf2f74a5a63389842e556d7cdc3a27b044941a
Munir Ali Mohammed <mamsheikh123@gmail.com>
2025-02-25 23:00:38 +0530

    feat: Message serialization & Error Handling

--- apps/jobber-executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
+++ apps/jobber-executor/src/app/jobs/fibonacci/fibonacci.consumer.ts
@@ -1,14 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { FibonacciData } from './fibonacci-data.interface';
+import { iterate } from 'fibonacci';
 import { PulsarClient, PulsarConsumer } from '@jobber/pulsar';
-import { Message } from 'pulsar-client';
 @Injectable()
-export class FibonacciConsumer extends PulsarConsumer implements OnModuleInit {
+export class FibonacciConsumer
+  extends PulsarConsumer<FibonacciData>
+  implements OnModuleInit
+{
   constructor(pulsarClient: PulsarClient) {
     super(pulsarClient, 'Fibnonacci');
   }
 
-  protected async onMessage(message: Message): Promise<void> {
-    console.log('FibonacciConsumer.onMessage');
-    await this.acknowledge(message);
+  protected async onMessage(data: FibonacciData): Promise<void> {
+    const result = iterate(data.iterations);
+    this.logger.log(result);
   }
 }
